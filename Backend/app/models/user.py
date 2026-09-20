f630635288550397e4754980c8b2a9b4826612cb
rajeshkanna-36 <your.email@gmail.com>
2026-09-20 12:29:45 +0530

    Add SQLAlchemy User model

---
+++ Backend/app/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import BigInteger, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "User"
+
+    user_id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        index=True
+    )
+
+    username: Mapped[str] = mapped_column(
+        Text,
+        unique=True,
+        nullable=False
+    )
