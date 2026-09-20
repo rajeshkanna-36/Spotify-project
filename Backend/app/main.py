02402868fbbcdf5ebe87b13d9d5292cf591283dd
rajeshkanna-36 <your.email@gmail.com>
2026-09-20 12:20:58 +0530

    Update main.py to match remote version

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from app.database.base import Base
+from app.database.connection import engine
+from app.models.user import User
+
 app = FastAPI()
 
-@app.get('/')
-def main():
-    return {"Sucess"}
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
