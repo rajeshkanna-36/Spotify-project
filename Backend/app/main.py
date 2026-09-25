af926f2321cd80d688a120bfa83cb977a0f5482d
rajeshkanna-36 <rajeshkanna.swe@gmail.com>
2026-09-25 13:03:24 +0530

    artist

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -9,6 +9,7 @@ from app.models import *
 from app.routes.auth import router as auth_router
 from app.routes.song import router as song_router
 from app.routes.album import router as album_router
+from app.routes.artist import router as artist_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -19,4 +20,5 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(song_router)
 app.include_router(album_router)
+app.include_router(artist_router)
 
